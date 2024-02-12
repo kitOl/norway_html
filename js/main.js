e0b470768f2ef05bf8bafcbfadb1b4b5bf14bc94
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-02-12 06:16:30 +0300

    add tinySlider to header

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// import { tns } from "./src/tiny-slider.js";
+
+const slider = tns({
+  container: ".header-slider",
+  items: 1,
+  slideBy: "page",
+  //   autoplay: true,
+  controls: false,
+  autoHeight: true,
+  //   nav: false,
+  //   arrowKeys: true,
+  //   navAsThumbnails: true,
+});
+
+const sliderPrev = document.querySelector("#sliderPrev");
+const sliderNext = document.querySelector("#sliderNext");
+
+sliderNext.onclick = function () {
+  slider.goTo("next");
+};
+sliderPrev.onclick = function () {
+  slider.goTo("prev");
+};
+
+const currentIndex = document.querySelector("#currentIndex");
+const totalIndex = document.querySelector("#totalIndex");
+
+function updateSliderFraction() {
+  const sliderInfo = slider.getInfo();
+  if (sliderInfo.index > sliderInfo.pages) {
+    currentIndex.innerText = "01";
+  } else if (sliderInfo.index === 0) {
+    currentIndex.innerText = "0" + sliderInfo.pages;
+  } else {
+    currentIndex.innerText = "0" + sliderInfo.index;
+  }
+
+  totalIndex.innerText = "0" + sliderInfo.pages;
+}
+
+updateSliderFraction();
+
+slider.events.on("indexChanged", () => {
+  updateSliderFraction();
+});
